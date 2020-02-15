183764c14e7f4ae086c2399bf80a6372b095fa1f
Averi Justice <justic35@msu.edu>
2020-02-14 21:12:09 -0500

    Fixed Movie Grid Layout

---
+++ Flix2/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix2
+//
+//  Created by Averi Justice on 2/14/20.
+//  Copyright © 2020 aaejustice. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
